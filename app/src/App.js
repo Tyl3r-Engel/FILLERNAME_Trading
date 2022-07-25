24e0a0a02b5c69ffe2a1453e1d2cafb2223de74b
Tyler Engel <engelcrag@gmail.com>
2022-07-25 15:00:39 -0700

    inital setup

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        this is main area
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
